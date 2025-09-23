088d16e22c64c490b452de1315a22ee7d9037eed
oz-create <ozlemengin4542@gmail.com>
2025-09-23 15:25:05 +0300

    feat: enhance MoviesSection and SeriesSection components; implement category filtering and update state management for improved data handling

--- movie-website/components/MoviesSection.tsx
+++ movie-website/components/MoviesSection.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import MovieCard from './MovieCard'
 import { useSelector } from 'react-redux';
 import { RootState } from '@/store/store';
@@ -6,10 +6,36 @@ import CategorySlider from './CategorySlider';
 import { BASE_URL } from '@/config/constants';
 import SeeMoreButton from './SeeMoreButton';
 import Link from 'next/link';
+import { ListTypeArray } from '@/types/type';
 
 
 export default function MoviesSection() {
   const { moviesCategories, nowPlayingMovies} = useSelector((state: RootState) => state.movies);
+  const [filteredData,setFilterData] = useState<ListTypeArray>(nowPlayingMovies)
+   const [selectedCategory, setSelectedCategory] = useState<number | null>(null);
+
+    useEffect(() => {
+      setFilterData(nowPlayingMovies)
+    }, [nowPlayingMovies])
+
+    function checkCategory(genreIds: number[]) {
+      if (selectedCategory === null) return true;
+      return genreIds.includes(selectedCategory);
+    }
+
+    const filter = () => {
+      const filtered = nowPlayingMovies.filter((element) => {
+        const categoryCheck = checkCategory(element.genre_ids)
+        return categoryCheck;
+      });
+
+      setFilterData(filtered);
+    };
+
+
+    useEffect(() => {
+      filter()
+    }, [selectedCategory])
 
   return (
     <section className='flex flex-col gap-10 mx-10 my-20'>
@@ -17,10 +43,10 @@ export default function MoviesSection() {
                 <h1 className='text-5xl text-[var(--color-primary)] font-bold'>Movies</h1>
                 <SeeMoreButton link="/movies" />
           </div>
-        <CategorySlider data={moviesCategories} />
-        <div className='flex items-center justify-start gap-5 overflow-x-scroll overflow-y-hidden max-w-[100%] py-5 px-2'>
+        <CategorySlider data={moviesCategories} setSelected={setSelectedCategory}/>
+        <div className='flex items-center justify-start gap-5 overflow-x-auto overflow-y-hidden max-w-[100%] py-5 px-2'>
             {
-                nowPlayingMovies.map((movie,index) => (
+                filteredData.map((movie,index) => (
                   <Link href={`/${movie.id}`} key={index}>
                     <MovieCard imageUrl={BASE_URL + movie.poster_path} />
                   </Link>
--- movie-website/components/SeriesSection.tsx
+++ movie-website/components/SeriesSection.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import MovieCard from './MovieCard'
 import { useSelector } from 'react-redux';
 import { RootState } from '@/store/store';
@@ -6,10 +6,37 @@ import CategorySlider from './CategorySlider';
 import { BASE_URL } from '@/config/constants';
 import SeeMoreButton from './SeeMoreButton';
 import Link from 'next/link';
+import { ListTypeArray } from '@/types/type';
 
 
 export default function SeriesSection() {
   const { seriesCategories, series } = useSelector((state: RootState) => state.movies);
+
+    const [filteredData,setFilterData] = useState<ListTypeArray>(series)
+     const [selectedCategory, setSelectedCategory] = useState<number | null>(null);
+  
+      useEffect(() => {
+        setFilterData(series)
+      }, [series])
+  
+      function checkCategory(genreIds: number[]) {
+        if (selectedCategory === null) return true;
+        return genreIds.includes(selectedCategory);
+      }
+  
+      const filter = () => {
+        const filtered = series.filter((element) => {
+          const categoryCheck = checkCategory(element.genre_ids)
+          return categoryCheck;
+        });
+  
+        setFilterData(filtered);
+      };
+  
+  
+      useEffect(() => {
+        filter()
+      }, [selectedCategory])
 
 
   return (
@@ -19,10 +46,10 @@ export default function SeriesSection() {
         <SeeMoreButton link="/series" />
       </div>
         
-        <CategorySlider data={seriesCategories} />
-        <div className='flex items-center justify-start gap-5 overflow-x-scroll overflow-y-hidden max-w-[100%] py-5 px-2'>
+        <CategorySlider data={seriesCategories} setSelected={setSelectedCategory}/>
+        <div className='flex items-center justify-start gap-5 overflow-x-auto overflow-y-hidden max-w-[100%] py-5 px-2'>
             {
-                series.map((movie,index) => (
+                filteredData.map((movie,index) => (
                   <Link href={`/${movie.id}`} key={index}>
                     <MovieCard imageUrl={BASE_URL + movie.poster_path} />
                   </Link>
